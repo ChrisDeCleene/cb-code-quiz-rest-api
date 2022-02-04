876d7ceafa8a8282252541b33c74dbf214c547f1
Chris DeCleene <chris_decleene@ymail.com>
2022-02-04 09:50:44 -0700

    Install and set up initial state for Swagger UI package swagger-ui-express

--- loaders/index.js
+++ loaders/index.js
@@ -1,19 +1,20 @@
 const expressLoader = require("./express");
 const routesLoader = require("../routes");
-const errorhandler = require('errorhandler')
+const swaggerLoader = require("./swagger");
+const errorhandler = require("errorhandler");
 
 // Connect app to exppress, passport, and all routes here
 module.exports = async (app) => {
-  
   // Load Express middlewares
   await expressLoader(app);
 
   // TODO Load Passport middleware (use returned app from express)
 
   // Load API route handlers
-  await  routesLoader(app);
-  
+  await routesLoader(app);
+
   //TODO Load Swagger
+  swaggerLoader(app);
 
   // Error handler
   app.use(errorhandler());
